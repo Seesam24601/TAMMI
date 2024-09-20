ea55dc946a411f3a5cc8eed9c66ba26f293b3e8a
Seesam24601 <charlieandseesam@gmail.com>
2024-09-20 18:49:21 -0400

    Assert that columns are integer-valued

--- functions/preflight.R
+++ functions/preflight.R
@@ -11,13 +11,56 @@ is_integer <- function(x, variable_name) {
     Nothing if x if round(x) == x is TRUE and does not throw an error. Otherwise, it 
     throws an error.
   "
+  error_message <- paste(variable_name, "must be an integer")
+
   tryCatch({
     if (round(x) != x) {
-      stop(paste(variable_name, "must be an integer"), call. = FALSE)
+      stop(error_message, call. = FALSE)
     }
   }, error = function(msg) {
-      stop(paste(variable_name, "must be an integer"), call. = FALSE)
+      stop(error_message, call. = FALSE)
     })
+}
+
+
+is_integer_col <- function(col, df_name, col_name) {
+  "
+  Parameters: 
+    col - Vector to be tested on
+    df_name - Name of the dataframe to be used in the error message
+    col_name - Name of the column to be used in the error mesage
+
+  Returns:
+    Nothing if x if round(x) == x is TRUE and does not throw an error for every element
+    of col. Otherwise, it throws an error.
+  "
+
+  error_message <- paste("The", col_name, "field in", df_name, "cannot contain non-integer values")
+  
+  tryCatch({
+    if (!all(sapply(col, (function(x) round(x) == x)))) {
+      stop(error_message, call. = FALSE)
+    }
+  }, error = function(msg) {
+    stop(error_message, call. = FALSE)
+  })
+}
+
+
+test_assets <- function(assets) {
+
+  # Assert year_built is integer-valued
+  is_integer_col(assets$year_built, "assets", "year_built")
+}
+
+
+test_asset_types <- function(asset_types) {
+
+  # Assert replacement_cost is integer-valued
+  is_integer_col(asset_types$replacement_cost, "asset_types", "replacement_cost")
+
+  # Assert useful_life is integer_valued
+  is_integer_col(asset_types$useful_life, "asset_types", "useful_life")
 }
 
 
--- models/unconstrained.R
+++ models/unconstrained.R
@@ -14,8 +14,20 @@ unconstrained <- function(assets,
                           end_year) {
   "
   Parameters:
-    assets
-    asset_types
+    assets - Dataframe containing one row for every asset. It must have the following
+      fields:
+        > asset_it: Must be unique
+        > asset_type_id: Must be present in asset_types$asset_type_id
+        > year_built: Year the asset was created. This is used to calculate the age of 
+            the asset. Cannot be greater than or equal to start_year. Must be an integer
+            value.
+    asset_types - Dataframe containing one row for each type of asset. It must have the
+      following fields:
+        > asset_types_id: Must be unique
+        > useful_life: The age in years that the asset must be replacement. Must be an
+            integer value
+        > replacement_cost: The cost to replace the asset in US dollars. Must be an
+            integer value.
     start_year - The first year the model calculates actions for. This should be an
       integer value. This should be <= end_year.
     end_year - That last year the model calculates actions for. This should be an
@@ -23,6 +35,10 @@ unconstrained <- function(assets,
 
   Returns:
   "
+  
+  # Assert that the assets dataframe meets its requirements
+
+  # Assert that the asset_types dataframe meets its requirements
 
   # Assert that start_year and end_year and integers and start_year <= end_year
   year_order(start_year, end_year)
