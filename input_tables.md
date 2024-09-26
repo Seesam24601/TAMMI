ebc832fd792399d4b2a8f9e2c38df67fe130f8ba
Charlie Murphy <CMurphy2@mta.maryland.gov>
2024-09-26 16:12:41 -0400

    Create input_tables documentation

---
+++ input_tables.md
@@ -0,0 +1,23 @@
+# Input Tables
+
+
+## assets
+
+Table that contains 1 row for every asset. An asset is a single individual object, like bus #24000.
+
+| ---- | ---- | ---- |
+| Field | Code | Description |
+| asset_it | PK | |
+| asset_type_id | FK | Keys into asset_type_id in asset_types table |
+| year_built | |  Year the asset was created. This is used to calculate the age of the asset. Cannot be greater than or equal to start_year. Must be an integer value. |
+
+
+# asset_types
+
+Table that contains 1 row for each type of asset. An asset type is a set of objects that all share the same replacement and rehab actions. For example, all 40 ft. buses would be expected to have the same rehab and replacement schedules.
+
+| ---- | ---- | ---- |
+| Field | Code | Description |
+| asset_type_id | PK | |
+| useful_life | | The age in years that the asset must be replacement. Must be an integer value |
+| replacement_cost | | The cost to replace the asset in US dollars. Must be an integer value. |
--- models/unconstrained.R
+++ models/unconstrained.R
@@ -18,20 +18,8 @@ unconstrained <- function(assets,
                           cost_adjustment = inflation) {
   "
   Parameters:
-    assets - Dataframe containing one row for every asset. It must have the following
-      fields:
-        > asset_it: Must be unique
-        > asset_type_id: Must be present in asset_types$asset_type_id
-        > year_built: Year the asset was created. This is used to calculate the age of 
-            the asset. Cannot be greater than or equal to start_year. Must be an integer
-            value.
-    asset_types - Dataframe containing one row for each type of asset. It must have the
-      following fields:
-        > asset_types_id: Must be unique
-        > useful_life: The age in years that the asset must be replacement. Must be an
-            integer value
-        > replacement_cost: The cost to replace the asset in US dollars. Must be an
-            integer value.
+    assets - see input_tables.md
+    asset_types - see input_tables.md
     start_year - The first year the model calculates actions for. This should be an
       integer value. This should be <= end_year.
     end_year - That last year the model calculates actions for. This should be an
