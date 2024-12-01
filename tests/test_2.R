269e51e73d688d008b426cf8be38e4dc3032ecc2
Charlie Murphy <charlie.murphy@kimley-horn.com>
2024-12-01 18:55:27 -0500

    Tests 3 through 6

--- tests/test_2.R
+++ tests/test_2.R
@@ -68,6 +68,7 @@ test_that(test_name, {
       asset_id = c(0, 1),
       asset_type_id = c(0, 0),
       action_id = c(0, 0),
+      budget_id = c(0, 0),
       cost = c(100, 100)
     )
   )
--- tests/test_6.R
+++ tests/test_6.R
@@ -14,16 +14,22 @@ asset_types <- tibble(
 )
 
 asset_actions <- tibble(
-  asset_action_id = c(0),
+  action_id = c(0),
   asset_type_id = c(0),
   age_trigger = c(5),
   cost = c(100),
   replacement_flag = c(1)
 )
 
-budget <- tibble(
-  year = 2000:2005,
-  budget = rep(1000)
+budgets <- tibble(
+  budget_id = c(0),
+  year = c(2000),
+  budget = c(1000)
+)
+
+budget_actions <- tibble(
+  action_id = c(0),
+  budget_id = c(0)
 )
 
 start_year <- 2000
@@ -44,7 +50,7 @@ test_that(test_name, {
       year = c(2000, 2005),
       asset_id = c(0, 0),
       asset_type_id = c(0, 0),
-      asset_action_id = c(0, 0),
+      action_id = c(0, 0),
       cost = c(100, 116)
     ),
     tolerance = 0.001
@@ -55,13 +61,15 @@ test_that(test_name, {
       asset_types, 
       asset_actions, 
       budget,
+      budget_actions,
       start_year, 
       end_year),
     tibble(
       year = c(2000, 2005),
       asset_id = c(0, 0),
       asset_type_id = c(0, 0),
-      asset_action_id = c(0, 0),
+      action_id = c(0, 0),
+      budget_id = c(0, 0),
       cost = c(100, 116)
     ),
     tolerance = 0.001
