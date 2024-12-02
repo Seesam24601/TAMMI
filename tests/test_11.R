e42d75d3a2ec3c82980f6eddd27e2cea5b38b2dc
Charlie Murphy <charlie.murphy@kimley-horn.com>
2024-12-01 19:26:51 -0500

    Create and pass tests 11 and 12 that deal with using multiple budgets

---
+++ tests/test_11.R
@@ -0,0 +1,75 @@
+test_name = "Test 11: 2 budgets, only one of which is used"
+
+
+# ---- Inputs ----
+
+assets <- tibble(
+  asset_id = c(0),
+  asset_type_id = c(0),
+  year_built = c(1990)
+)
+
+asset_types <- tibble(
+  asset_type_id = c(0)
+)
+
+asset_actions <- tibble(
+  action_id = c(0),
+  asset_type_id = c(0),
+  age_trigger = c(5),
+  cost = c(100),
+  replacement_flag = c(1)
+)
+
+budgets <- tibble(
+  budget_id = c(0, 1),
+  year = c(2000, 2000),
+  budget = c(1000, 50)
+)
+
+budget_actions <- tibble(
+  action_id = c(0),
+  budget_id = c(0)
+)
+
+start_year <- 2000
+end_year <- 2000
+
+
+# ---- Test -----
+
+test_that(test_name, {
+  expect_equal(
+    traditional_run(
+      assets, 
+      asset_types, 
+      asset_actions, 
+      budgets,
+      budget_actions,
+      start_year, 
+      end_year),
+    tibble(
+      year = c(2000),
+      asset_id = c(0),
+      asset_type_id = c(0),
+      action_id = c(0),
+      budget_id = c(0),
+      cost = c(100)
+    )
+  )
+})
+
+
+# ---- Close ---
+
+# Remove all objects to they don't affect subsequent tests
+rm(list = c(
+  "test_name",
+  "assets",
+  "asset_types",
+  "asset_actions",
+  "budgets",
+  "budget_actions",
+  "start_year",
+  "end_year"
+))
---
+++ tests/test_12.R
@@ -0,0 +1,85 @@
+test_name = "Test 12: 2 budgets, each assigned to a different action"
+
+
+# ---- Inputs ----
+
+assets <- tibble(
+  asset_id = c(0),
+  asset_type_id = c(0),
+  year_built = c(1995)
+)
+
+asset_types <- tibble(
+  asset_type_id = c(0)
+)
+
+asset_actions <- tibble(
+  action_id = c(0, 1),
+  asset_type_id = c(0, 0),
+  age_trigger = c(5, 10),
+  cost = c(100, 500),
+  replacement_flag = c(0, 1)
+)
+
+budgets <- tibble(
+  budget_id = c(0, 1),
+  year = c(2000, 2000),
+  budget = c(1000, 1000)
+)
+
+budget_actions <- tibble(
+  action_id = c(0, 1),
+  budget_id = c(0, 1)
+)
+
+start_year <- 2000
+end_year <- 2005
+
+# Dummy function to ignore inflation
+cost_adjustment_dummy <- function(
+  asset_details,
+  current_year,
+  start_year
+) {
+  asset_details
+}
+
+
+# ---- Test -----
+
+test_that(test_name, {
+  expect_equal(
+    traditional_run(
+      assets, 
+      asset_types, 
+      asset_actions, 
+      budgets,
+      budget_actions,
+      start_year, 
+      end_year, 
+      cost_adjustment = cost_adjustment_dummy),
+    tibble(
+      year = c(2000, 2005),
+      asset_id = c(0, 0),
+      asset_type_id = c(0, 0),
+      action_id = c(0, 1),
+      budget_id = c(0, 1),
+      cost = c(100, 500)
+    )
+  )
+})
+
+
+# ---- Close ---
+
+# Remove all objects to they don't affect subsequent tests
+rm(list = c(
+  "test_name",
+  "assets",
+  "asset_types",
+  "asset_actions",
+  "budgets",
+  "budget_actions",
+  "start_year",
+  "end_year"
+))
