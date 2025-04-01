c9567814ca1a2ab1ae82d4dbba3efc070b830540
Charlie Murphy <cmurphy2@mdot.maryland.gov>
2025-04-01 10:13:13 -0400

    Convert models to objects

--- models/traditional.R
+++ models/traditional.R
@@ -282,5 +282,14 @@ traditional_run <- function(
   }
 
   # Combine all years worth of replacements into a single dataframe
-  do.call(bind_rows, actions)
+  performed_actions <- do.call(bind_rows, actions)
+
+  # Create a single object with all the results
+  result <- list(
+    performed_actions = performed_actions
+  )
+
+  class(result) <- "traditional_tammi_model"
+
+  return(result)
 }
