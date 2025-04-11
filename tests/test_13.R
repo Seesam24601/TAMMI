54651d8801f2fc08f8fe4e75b40aa535d3fc21b3
Charlie Murphy <cmurphy2@mdot.maryland.gov>
2025-04-11 10:59:48 -0400

    Update additional test cases

--- tests/test_13.R
+++ tests/test_13.R
@@ -28,11 +28,11 @@ budgets <- tibble(
 budget_years <- tibble(
   budget_id = rep(0),
   year = 2000:2005,
-  budget = c(1000, 0, 0, 0, 0, 0)
+  budget = c(100, 0, 0, 0, 0, 0)
 )
 
 budget_actions <- tibble(
-  action_id = c(0, 0),
+  action_id = c(0, 1),
   budget_id = c(0, 0)
 )
 
@@ -71,6 +71,26 @@ test_that(test_name, {
       action_id = c(0),
       budget_id = c(0),
       cost = c(100)
+    )
+  )
+  expect_equal(
+    traditional_run(
+      assets, 
+      asset_types, 
+      asset_actions, 
+      budgets,
+      budget_years,
+      budget_actions,
+      start_year, 
+      end_year,
+      cost_adjustment = cost_adjustment_dummy
+    )$backlog,
+    tibble(
+      year = c(2005),
+      asset_id = c(0),
+      asset_type_id = c(0),
+      action_id = c(1),
+      cost = c(500)
     )
   )
 })
